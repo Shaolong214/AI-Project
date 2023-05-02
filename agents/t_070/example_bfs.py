ed2ed22d111cd86efd667c6a963910894815be98
snnallwh1te <ljy980721@gmail.com>
2023-05-02 19:59:12 +1000

    Update BFS Agent
    Goal : number of tiles taken matches the pattern line

--- agents/t_070/example_bfs.py
+++ agents/t_070/example_bfs.py
@@ -16,10 +16,6 @@ from collections import deque
 import Azul.azul_utils as utils
     
 
-import Azul.azul_model
-import Azul.azul_utils as utils
-from   template     import GameState
-
 THINKTIME   = 0.9
 NUM_PLAYERS = 2
 
@@ -70,9 +66,7 @@ class myAgent():
                 next_path  = path + [a]                   # Add this action to the path.
                 goal     = self.DoAction(next_state, a) # Carry out this action on the state, and check for goal
                 if goal:
-
                     print("path found:",next_path[0])
-
                     return next_path[0] # If the current action reached the goal, return the initial action that led there.
                 else:
                     queue.append((next_state, next_path)) # Else, simply add this state and its path to the queue.
