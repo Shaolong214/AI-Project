909ed315f0898901433987d66045105a0f679470
Shaolong214 <103941617+Shaolong214@users.noreply.github.com>
2023-05-19 17:17:35 +1000

    final_approx_q

--- agents/t_070/MCTS.py
+++ agents/t_070/MCTS.py
@@ -2,36 +2,48 @@ import copy
 import random
 
 
-self.children={}
 
-def select(self,state, ):
-        # If the current node is not fully expanded, return the node itself
-        if not self.isFullyExpanded(state):
-            return self
+class MCTS:
+    def select(self,state, ):
+            # If the current node is not fully expanded, return the node itself
+            if not self.isFullyExpanded(state):
+                return self
 
-        # If the node is fully expanded, proceed to select from its children based on epsilon-greedy strategy
-        else:
-            actions = list(self.children.keys())
-            qValues = [self.children[action].getValue() for action in actions]
-
-            # Select an action randomly with 10% chance (exploration)
-            if random.random() < 0.05:
-                selected_action = random.choice(actions)
-            # Otherwise select the action with the highest Q value (exploitation)
+            # If the node is fully expanded, proceed to select from its children based on epsilon-greedy strategy
             else:
-                max_q_value = max(qValues)
-                best_actions = [action for action, q_value in zip(actions, qValues) if q_value == max_q_value]
+                actions = list(self.children.keys())
+                qValues = [self.children[action].getValue() for action in actions]
 
-                # If there are no best actions (when the game ends), return None
-                if len(best_actions) == 0:
-                    return None
+                # Select an action randomly with 10% chance (exploration)
+                if random.random() < 0.05:
+                    selected_action = random.choice(actions)
+                # Otherwise select the action with the highest Q value (exploitation)
+                else:
+                    max_q_value = max(qValues)
+                    best_actions = [action for action, q_value in zip(actions, qValues) if q_value == max_q_value]
 
-                # Randomly select one of the best actions
-                selected_action = random.choice(best_actions)
+                    # If there are no best actions (when the game ends), return None
+                    if len(best_actions) == 0:
+                        return None
 
-            # Get the child node corresponding to the selected action and recursively call select function on it
-            child = self.children[selected_action]
-            return child.select(child.state)
+                    # Randomly select one of the best actions
+                    selected_action = random.choice(best_actions)
+
+                # Get the child node corresponding to the selected action and recursively call select function on it
+                child = self.children[selected_action]
+                return child.select(child.state)
+
+    def backPropagate(self, reward):
+        # Increment the visit count for this node
+        self.visits += 1
+
+        # Update the value of this node. The new value is a weighted average of the current value and the new reward.
+        self.value += (self.reward + reward - self.value) / self.visits
+
+        # If this node has a parent, propagate the reward (discounted by 0.9) up to the parent.
+        if self.parent is not None:
+            self.parent.backPropagate(reward * 0.9)  # Apply a discount factor of 0.9
+
 
 
  
--- agents/t_070/test.py
+++ agents/t_070/test.py
@@ -1,4 +1,5 @@
 from Azul.azul_model import AzulGameRule as GameRule
+import Azul.azul_utils as utils
 import json
 import os
 import random
@@ -11,40 +12,8 @@ FLOOR_SCORES = [-1,-1,-2,-2,-2,-3,-3]
 ROW_BONUS = 2
 COL_BONUS = 7
 SET_BONUS = 10
-grid_state = numpy.zeros((GRID_SIZE,GRID_SIZE))
 
 class myAgent:
-    # def run():
-    #     # Initialize the game and the agents
-    #     game_rule = GameRule(NUM_PLAYERS)
-    #     agents = [myAgent(i) for i in range(NUM_PLAYERS)]
-
-    #     # Initial game state
-    #     game_state = game_rule.getInitialState()
-
-    #     # Number of episodes to run
-    #     num_episodes = 100
-
-    #     for episode in range(num_episodes):
-    #         # Load weights at the start of each episode
-    #         for agent in agents:
-    #             agent.load_weights()
-
-    #         # Reset the game state at the start of each episode
-    #         game_state = game_rule.getInitialState()
-
-    #         # Play the game until it's over
-    #         while not game_rule.isGameOver(game_state):
-    #             for agent in agents:
-    #                 actions = agent.get_legal_actions(game_state)
-    #                 action = agent.SelectAction(actions, game_state)
-    #                 game_state = game_rule.generateSuccessor(game_state, action, agent.id)
-
-    #         # After the game is over, save the weights
-    #         for agent in agents:
-    #             agent.save_weights()
-
-    #         print(f"Episode {episode + 1} completed.")
 
     def __init__(self, _id):
         self.id = _id 
@@ -53,13 +22,18 @@ class myAgent:
         self.alpha = 0.5  # learning rate
         self.discount = 0.9  # discount factor
         self.train_model = True
+        self.epsilon = 1.0  # initial exploration rate
+        self.epsilon_min = 0.01  # minimum exploration rate
+        self.epsilon_decay = 0.995  # decay rate for exploration
         
-        # save the weight after training one episode (one round of game), then load the weight use to train second 
-        # episode (or in next round of game)
- # weight vector for every feature
+        self.grid_state = numpy.zeros((GRID_SIZE,GRID_SIZE))
 
         self.weights_file = 'weights.json'
-        self.weights = self.load_weights()  # Load weights from file if available
+        self.weights = self.load_weights()  # Load weights 
+
+        self.number_of = {}
+        for tile in utils.Tile:
+            self.number_of[tile] = 0
 
         if self.weights is None:
             self.weights = {}
@@ -81,134 +55,76 @@ class myAgent:
         """Get all legal actions for the state."""
         return self.game_rule.getLegalActions(state, self.id)
     
-# agent_state.grid_state[pattern_line]
-    # def GetCompletedRows(game_state):
-    #         completed = 0
-    #         for i in range(GRID_SIZE):
-    #             allin = True
-    #             for j in range(GRID_SIZE):
-    #                 if game_state.grid_state[i][j] == 0:
-    #                     allin = False
-    #                     break
-    #             if allin:
-    #                 completed += 1
-    #         return completed
-
-        # Compute number of completed columns in the agent's grid
-    # def GetCompletedColumns(self):
-    #         completed = 0
-    #         for i in range(self.GRID_SIZE):
-    #             allin = True
-    #             for j in range(self.GRID_SIZE):
-    #                 if self.grid_state[j][i] == 0:
-    #                     allin = False
-    #                     break
-    #             if allin:
-    #                 completed += 1
-    #         return completed
-
-    #     # Compute the number of completed tile sets in the agent's grid
-    # def GetCompletedSets(self):
-    #         completed = 0
-    #         for tile in utils.Tile:
-    #             if self.number_of[tile] == self.GRID_SIZE:
-    #                 completed += 1
-    #         return completed
-
-
     def extract_features(self, game_state, action):
         """Extract useful features from the game state and action."""
         next_game_state = self.game_rule.generateSuccessor(copy.deepcopy(game_state), action, self.id)
-
         current_agent_state = game_state.agents[self.id]
-        # lines_number store tiles number of each pattern line
-        current_num_completed_pattern_line = sum(1 for i in range(current_agent_state.GRID_SIZE) 
-                                                 if current_agent_state.lines_number[i] == i + 1)
-
         next_agent_state = next_game_state.agents[self.id]
+        # Update local grid state and tile count
+        self.grid_state = next_agent_state.grid_state
+        self.number_of = next_agent_state.number_of
+
+        def calculate_bonuses(state):
+            completed_row = 0
+            bonus_row = 0
+            for i in range(GRID_SIZE):
+                if all(state.grid_state[i][j] != 0 for j in range(GRID_SIZE)):
+                    completed_row += 1
+                    bonus_row = completed_row * ROW_BONUS
+
+            bonus_col = 0
+            completed_col = 0
+            for i in range(GRID_SIZE):
+                if all(state.grid_state[j][i] != 0 for j in range(GRID_SIZE)):
+                    completed_col += 1
+                    bonus_col = completed_col * COL_BONUS
+
+            bonus_set = 0 
+            completed_set = 0
+            for tile in utils.Tile:
+                if state.number_of[tile] == GRID_SIZE:
+                    completed_set += 1
+                    bonus_set = completed_set * SET_BONUS
+
+            return bonus_row + bonus_col + bonus_set
+
+        current_bonus = calculate_bonuses(current_agent_state)
+        next_bonus = calculate_bonuses(next_agent_state)
+
+        current_num_completed_pattern_line = sum(1 for i in range(current_agent_state.GRID_SIZE) 
+                                                if current_agent_state.lines_number[i] == i + 1)
+        
         next_num_completed_pattern_line = sum(1 for i in range(next_agent_state.GRID_SIZE) 
-                                              if next_agent_state.lines_number[i] == i + 1)
-        # lost scores according to the number of tiles on floor
+                                            if next_agent_state.lines_number[i] == i + 1)
+        
         current_floor_tiles_score = sum(floor_score for floor_tile_exists, floor_score 
                                         in zip(game_state.agents[self.id].floor,
-                                               game_state.agents[self.id].FLOOR_SCORES) if floor_tile_exists == 1)
+                                            game_state.agents[self.id].FLOOR_SCORES) if floor_tile_exists == 1)
 
         next_floor_tiles_score = sum(floor_score for floor_tile_exists, floor_score 
-                                     in zip(next_game_state.agents[self.id].floor,
+                                    in zip(next_game_state.agents[self.id].floor,
                                             next_game_state.agents[self.id].FLOOR_SCORES) if floor_tile_exists == 1)
-        completed = 0
-        current_bonus = 0
-        for i in range(GRID_SIZE):
-            allin = True
-            for j in range(GRID_SIZE):
-                if current_agent_state.grid_state[i][j] == 0:
-                    allin = False
-                    break
-            if allin:
-                completed += 1
-                current_bonus = completed*ROW_BONUS
-
-        
-#         # cur_cols = current_agent_state.GetCompletedColumns()
-#         # cur_sets = current_agent_state.GetCompletedSets()
-#         cur_rows = current_agent_state.GetCompletedRows()
-#         current_bonus =  (cur_rows * current_agent_state.ROW_BONUS)
-# # (cur_cols * current_agent_state.COL_BONUS) + (cur_sets * current_agent_state.SET_BONUS) +
-#         # next_cols = next_agent_state.GetCompletedColumns()
-#         # next_sets = next_agent_state.GetCompletedSets()
-#         next_rows = next_agent_state.GetCompletedRows()
-#         next_bonus = (next_rows * next_agent_state.ROW_BONUS)
-# # (next_cols * next_agent_state.COL_BONUS) + (next_sets * next_agent_state.SET_BONUS) + 
-
-
-            
     
-        # next_completed = 0
-        # next_bonus = 0
-        # for i in range(GRID_SIZE):
-        #     allin = True
-        #     for j in range(GRID_SIZE):
-        #         if next_agent_state.grid_state[i][j] == 0:
-        #             allin = False
-        #             break
-        #     if allin:
-        #         next_completed += 1
-        #         next_bonus = next_completed*ROW_BONUS
-            
-
-        # the value of returned feature is f_value
         return {
             'complete_pattern_lines_added': next_num_completed_pattern_line - current_num_completed_pattern_line,
             'floor_score_change': next_floor_tiles_score - current_floor_tiles_score,
-            'bonus_change': current_bonus 
-        }
-
-    # def get_q_value(self, state, action):
-    #     """Calculate the Q value."""
-    #     features = self.extract_features(state, action) # get features info of the a state by an action
-    #     # initialise accumulative sum of the product between each weights and each features.
-    #     return sum(self.weights.get(feature, 0) * f_value for feature, f_value in features.items())
+            'bonus_change': next_bonus - current_bonus}
     
     def get_q_value(self, state, action):
+        """Calculate the Q-value for a given state and action."""
+
+        # Extract the features from the current state-action pair
         features = self.extract_features(state, action)
-        sum = 0
+
+        # Initialize the Q-value sum
+        q_value = 0
+
+        # For each feature, add the product of the feature's value and its corresponding weight to the sum
         for feature, f_value in features.items():
             if feature in self.weights:
-                sum += self.weights[feature] * f_value
-        return sum
-
-
-    # def SelectAction(self, actions, game_state):
-    #     if not self.train_model:
-    #         return self.best_action(game_state)
-        
-    #     action = random.choice(self.get_legal_actions(game_state)) if random.random() < 0.1 else self.best_action(game_state)
-        
-    #     new_state = self.game_rule.generateSuccessor(copy.deepcopy(game_state), action, self.id)
-    #     self.update_weight_vector(game_state, action, new_state, self.calculate_reward(self))
-        
-    #     return action
-
+                q_value += self.weights[feature] * f_value
+        return q_value
+    
     def SelectAction(self, actions, game_state):
         # If not in training mode, simply return the best action
         if not self.train_model:
@@ -218,7 +134,7 @@ class myAgent:
             
             random_number = random.random()
 
-            if random_number < 0.1:
+            if random_number < self.epsilon:
                 # Explore: select a random action
                 legal_actions = self.get_legal_actions(game_state)
                 action = random.choice(legal_actions)
@@ -234,54 +150,43 @@ class myAgent:
 
             # Update weight vector based on the action's reward
             self.update_weight_vector(game_state, action, new_state, reward)
-
-            #if end of round, save
-            #if start of round, load
+            
+            # Decay the epsilon after each action
+            if self.epsilon > self.epsilon_min:
+                self.epsilon *= self.epsilon_decay
             
             # Return the chosen action
             return action
 
     def update_weight_vector(self, state, action, next_state, reward):
-        """Update the weight vector."""
+        """Update the weight vector based on the given state and action."""
+
+        # Load current weights
         self.weights = self.load_weights()
+
+        # Get legal actions in the next state and their corresponding Q-values
         nextstep_actions = self.get_legal_actions(next_state)
-        nextstep_q_values = [self.get_q_value(next_state, a) for a in nextstep_actions] #get q value for every states by each legal action
+        nextstep_q_values = [self.get_q_value(next_state, a) for a in nextstep_actions]
+
+        # Find the maximum Q-value in the next step, defaulting to 0 if there are no actions
         max_q_next_step = max(nextstep_q_values, default=0)
-        # Formula provided on lecture, get the new information of new state
+
+        # Calculate the difference between the updated Q-value estimate and the old Q-value
         delta = reward + self.discount * max_q_next_step - self.get_q_value(state, action)
-        # update weights of each feature value by a 'for' loop.
+
+        # Extract the features from the current state-action pair
         features = self.extract_features(state, action)
+
+        # Update the weight of each feature
         for feature_name, f_value in features.items():
-            # assign 0 to weight if have not visited before
+            # If the feature hasn't been encountered before, its weight is assumed to be 0
             self.weights[feature_name] = self.weights.get(feature_name, 0) + self.alpha * delta * f_value
+
+        # Save the updated weights
         self.save_weights()
+
+        # Print the updated weights for debugging
         print(self.weights)
-
-    # def update_weight_vector(self, state, action, next_state, reward):
-    #     """Update the weight vector."""
-
-    #     # Load weights from the json file if it exists
-    #     if os.path.exists(self.weights_file):
-    #         with open(self.weights_file, 'r') as f:
-    #             self.weights = json.load(f)
-
-    #     nextstep_actions = self.get_legal_actions(next_state)
-    #     nextstep_q_values = [self.get_q_value(next_state, a) for a in nextstep_actions] #get q value for every states by each legal action
-    #     max_q_next_step = max(nextstep_q_values, default=0)
-
-    #     delta = reward + self.discount * max_q_next_step - self.get_q_value(state, action)
-
-    #     features = self.extract_features(state, action)
-    #     for feature_name, f_value in features.items():
-    #         # assign 0 to weight if have not visited before
-    #         self.weights[feature_name] = self.weights.get(feature_name, 0) + self.alpha * delta * f_value
-
-    #     # Save the weights to a json file
-    #     with open(self.weights_file, 'w') as f:
-    #         json.dump(self.weights, f)
-
-    #     print(self.weights)
-
 
     def best_action(self, state):
         """Select the action with the maximum Q(s, a), if multiple, choose one at random."""
@@ -294,29 +199,6 @@ class myAgent:
         if not best_actions:
             return None
         return random.choice(best_actions)
-
-
-    # def calculate_reward(self, game_state, action): 
-    #     """Calculate the reward based on the extracted features."""
-    #     reward = 0        
-    #     # current_agent_state = game_state.agents[self.id]
-
-    #     # Extract the features
-    #     curr_features = self.extract_features(game_state, action)
-
-    #     # Reward for each one of complete_pattern_lines_added that greater than 1 
-    #     if curr_features['complete_pattern_lines_added'] > 1:
-    #         reward += curr_features['complete_pattern_lines_added']
-        
-    #     # Reward for each one of floor_score_change greater than 1 
-    #     if curr_features['floor_score_change'] > 1:
-    #         reward -= curr_features['floor_score_change']
-        
-    #     # Reward for each 'bonus_change' that greater than 1
-    #     if curr_features['bonus_change'] > 1:
-    #         reward += curr_features['bonus_change']
-            
-    #     return reward
 
     def calculate_reward(self, game_state, action): 
         """Calculate the reward based on the extracted features."""
