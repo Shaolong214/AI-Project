074760316791f391907bbfce1c0e961d087ae734
Bocong Zhao <bocongzhao@163.com>
2023-05-21 05:34:05 +1000

    Add comments

--- agents/t_070/blindSearch.py
+++ agents/t_070/blindSearch.py
@@ -18,21 +18,29 @@ NUM_PLAYERS = 2
 # [Source code]: https://github.com/COMP90054-2023S1/assignment1-BocongZhao823/blob/2cec9b32e89803b6869496e3268120c3b796dd59/util.py#L133 
 class myStack():
     def __init__(self):
+        # Create an empty list which contains future nodes
         self.stack = []
 
     def push(self, state, action):
+        # A node shall contains state & action information
         node = state, action
+        # Add that node to the list
         self.stack.append(node)
 
     def pop(self):
+        # Is so far the list is not empty
         if self.is_empty() != True:
+            # Pop out the node from the list
             node = self.stack.pop()
+            # Unpage the node to state & action information
             state, action = node
             return state, action 
         else:
+            # If there is no node in the list
             print("No node left over.")
     
     def is_empty(self):
+        # If there is no node in the list
         if len(self.stack) == 0:
             return True
         else:
@@ -60,7 +68,6 @@ class myAgent():
             color_of_tiles = tile_grab.tile_type
             put_pattern_num = tile_grab.num_to_pattern_line
             patternLine_index = tile_grab.pattern_line_dest + 1
-            factory_index = action[1] + 1
             put_foor_num = tile_grab.num_to_floor_line
 
             # if no tile put on floor
@@ -91,24 +98,42 @@ class myAgent():
     
     def SelectAction(self, actions, rootstate):
         start_time = time.time()
-        stackWhole = myStack() # DFS use Stack 
+        # DFS use Stack 
+        stackWhole = myStack() 
+        # Initial a close list to contain vistied nodes
         closed = []
+        # Initial goal to false, means haven't achieve the goal yet
         goal = False
+        # For each initial action in the action list
         for initial_action in actions:  
+            # add the node with different actions as nodes to stack
             stackWhole.push(rootstate, initial_action)
+        # If so far goal is not reached; stack is not empty; time is not running out.
         if goal == False and stackWhole.is_empty() != True and time.time() - start_time < THINKTIME:
+            # Pop out one node from the stack with state & action information
             currentState, currentAction = stackWhole.pop()
+            # If I have not visit this node yet
             if currentState not in closed:
+                # visit the node now and add it to closed list
                 closed.append(currentState)
-                new_actions = self.GetActions(currentState) 
-                for action in new_actions:
-                    goal = self.checkGoal(currentState, action) 
-                    if goal == False:
+                # Check if this node reach the goal or not
+                goal = self.checkGoal(currentState, currentAction) 
+                # If this node does not reach the goal
+                if goal == False:
+                    # Expand this node to a new state
+                    newState = self.get_success(currentState,currentAction)
+                    # Find the according new actions for that new state
+                    new_actions = self.GetActions(newState) 
+                    # For each new action inside the new actions list
+                    for action in new_actions:
+                        # add the new state and new action to stack
                         stackWhole.push(currentState, action)
+                        # randomly choose an action
                         randomPath = random.choice(actions)
-                        return randomPath
-                    else:
-                        return action
+                    return randomPath
+                else:
+                    # If the goal is reached, reach the goal
+                    return action
     # End-----------------------------------------------------------------------
         
     
